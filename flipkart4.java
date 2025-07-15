5f40b626f19883189cf10946491b1f092f485881
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-15 14:21:05 +0530

    modified F by imran

--- flipkart4.java
+++ flipkart4.java
@@ -2,7 +2,7 @@ public class Flipkart4
 {
 	public static void main(String [] args)
 	{
-		System.out.println("hello world args of Flipkart4);
+		System.out.println("hello world args of flipkart4);
 	}
 }
 
