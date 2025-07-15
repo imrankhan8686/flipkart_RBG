a3ddbd595c6f85763ec6908fbf06470ce954d00b
imrankhan8686 <imrankhan.arbz@gmail.com>
2025-07-15 13:38:27 +0530

    adding files

---
+++ flipkart.java
@@ -0,0 +1,8 @@
+public class flipkart_login
+{
+public static void main(String [] args )
+{
+System.out.println("hello");
+}
+}
+
---
+++ flipkart3.java
@@ -0,0 +1,8 @@
+public class flipkart3
+{
+	public static void main (String [] args)
+	{
+		Sysytem.out.println("hello world args of flipkart3);
+	}
+}
+
